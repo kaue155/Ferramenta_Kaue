630eb1a1fea368eb28285b36359a7225f3651de7
kaue155 <00001094721803sp@al.educacao.sp.gov.br>
2024-11-18 21:56:51 -0300

    Add files via upload

---
+++ ferramenta de estudo kaue/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'matematica',
+    'O que é matematica?'
+    'matematica pode ajudar no oque?'
+
+criaCartao(
+    'Geografia'
+    'geografia pode ajudar no oque?'
+    'geografia ajuda nos graficos?'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
